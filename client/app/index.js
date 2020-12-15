695cd4445030f283379c678843aa52ac74eb1d84
SARAH SANTANA PEREIRA <sarah.pereira-ext@viavarejo.com.br>
2020-12-15 19:56:58 -0300

    Adicionando nova linha na tabela de negociações assim que uma negociação é adicionada pelo formulario

--- client/app/index.js
+++ client/app/index.js
@@ -1,3 +1,4 @@
+// referencias dos campos do formulario
 var campos = [
     document.querySelector('#data'),
     document.querySelector('#valor'),
@@ -6,8 +7,28 @@ var campos = [
 
 console.log(campos);
 
+// referencia da tabela de negociações
 var tbody = document.querySelector('table tbody');
 
 document.querySelector('.form').addEventListener('submit', function(event) {
-    alert('oi');
+
+    // cancela submição do formulario
+    event.preventDefault();
+
+    // adicionando linha na tabela para a nova negociação
+    var tr = document.createElement('tr');
+
+    // adicionando colunas para cada item da negociacao
+    campos.forEach(function(campo) {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    // adicionando coluna que guarda volume
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
 });
