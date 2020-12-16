1e3418a08666daa60f98722ed4cc08d39e563f9a
SARAH SANTANA PEREIRA <sarah.pereira-ext@viavarejo.com.br>
2020-12-16 19:20:07 -0300

    limpando o formulario

--- client/app/index.js
+++ client/app/index.js
@@ -31,4 +31,9 @@ document.querySelector('.form').addEventListener('submit', function(event) {
     tr.appendChild(tdVolume);
 
     tbody.appendChild(tr);
+
+    campos[0].value = '';
+    campos[1].value = 1;
+    campos[2].value = 0;
+    campos[0].focus();
 });
